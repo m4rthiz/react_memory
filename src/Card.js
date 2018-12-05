0e26f73d42cd245b7602caa98cb7c32cb46f3465
marie.thizy <marie.thizy@emse.fr>
2018-12-05 16:36:00 +0100

    add card and guesses component

--- src/Card.js
+++ src/Card.js
@@ -3,9 +3,15 @@ import React from 'react'
 
 import './Card.css'
 
-const HIDDEN_SYMBOL = '❓'
+const HIDDEN_SYMBOL = '❓';
 
-const Card = ({card, feedback}) => <div className="card" />
+const Card = ({card, feedback}) =>(
+    <div className={`card ${feedback}`}>
+        <span className="symbol">
+      {feedback === 'hidden' ? HIDDEN_SYMBOL : card}
+    </span>
+    </div>
+)
 
 
 
