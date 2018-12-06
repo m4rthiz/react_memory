bc7f197e155c17167c286294dbddfd77545c859e
marie.thizy <marie.thizy@emse.fr>
2018-12-06 10:57:06 +0100

    correction app => this dans la classe App.js

--- src/App.js
+++ src/App.js
@@ -3,20 +3,32 @@ import './App.css';
 import GuessCount from "./GuessCount";
 import Card from "./Card";
 
+
+
+
 class App extends Component {
+
+
     render() {
         return(
             <div className="memory">
             <GuessCount guesses={0}/>
-                <Card card="😀" feedback="hidden"/>
-                <Card card="🎉" feedback="justMatched" />
-                <Card card="💖" feedback="justMismatched" />
-                <Card card="🎩" feedback="visible" />
-                <Card card="🐶" feedback="hidden" />
-                <Card card="🐱" feedback="justMatched" />
+                <Card card="😀" feedback="hidden" onClick={this.handleCardClick}/>
+                <Card card="🎉" feedback="justMatched" onClick={this.handleCardClick}/>
+                <Card card="💖" feedback="justMismatched" onClick={this.handleCardClick}/>
+                <Card card="🎩" feedback="visible" onClick={this.handleCardClick}/>
+                <Card card="🐶" feedback="hidden" onClick={this.handleCardClick}/>
+                <Card card="🐱" feedback="justMatched" onClick={this.handleCardClick}/>
         </div>
         )
     }
+
+    handleCardClick(card) {
+        console.log(card, 'clicked')
+    }
+
 }
+
+
 
 export default App;
